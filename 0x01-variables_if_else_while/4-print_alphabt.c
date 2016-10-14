cdaebd8fccf99537d20490e924ce88c468ca3f45
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-14 02:17:57 +0000

    added c program to test putchar()

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NL '\n' 
+
+int main(void)
+{
+    char c;
+
+    c =  'a';
+    while(c <= 'z')
+    {
+        if (c == 'e')
+        {
+	     c = c+1;
+	}
+
+	else if (c == 'q')
+	{
+	     c = c+1;
+	}
+
+	putchar(c);
+        c = c+1;  
+    }
+
+    putchar(NL);
+    return 0;
+}
