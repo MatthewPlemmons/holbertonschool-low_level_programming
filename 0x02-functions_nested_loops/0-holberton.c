e87913e531c869d6cf1dee1cbb7dd0bc14b30205
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-18 23:15:16 +0000

    added _putchar program

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "_putchar.c"
+
+/**
+ * main - prints "Holberton" to stdout
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n[9] = "Holberton";
+	int i = 0;
+
+	while (i <= 8)
+	{
+		_putchar(n[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
