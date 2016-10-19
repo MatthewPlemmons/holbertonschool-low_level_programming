ed5758b310cd2b2c57898d19e4d0b25f7efb49f2
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-19 06:08:24 +0000

    added program to check for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for a lower-case character.
+ *
+ * Return: 1, if lower-case.
+ *         0, otherwise.
+ */
+
+int _islower(int c)
+{
+	unsigned char lower_case_a = 'a';
+	unsigned char lower_case_z = 'z';
+
+	if ((c < 0) || (c > 255))
+	{
+		return (0);
+	}
+	else if ((c >= lower_case_a) && (c <= lower_case_z))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,6 +8,14 @@
 
 int main(void)
 {
-	print_alphabet_x10();
+	int r;
+
+	r = _islower(48);
+	_putchar(r + '0');
+	r = _islower(-5);
+	_putchar(r + '0');
+	r = _islower('h');
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
