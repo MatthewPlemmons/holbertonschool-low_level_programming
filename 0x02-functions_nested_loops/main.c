f2e2807454e0332825824db9b3884e81f08939c3
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-19 07:50:03 +0000

    added print_sign() function

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -10,11 +10,24 @@ int main(void)
 {
 	int r;
 
-	r = _islower(48);
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
-	r = _islower(-5);
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
-	r = _islower('h');
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
