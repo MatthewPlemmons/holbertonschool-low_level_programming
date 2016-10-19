149ec16582a1956502ce127c77b0be5d365b909a
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-19 04:42:14 +0000

    added program that prints alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet on a new line ten times.
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+	return;
+}
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+	return;
+}
