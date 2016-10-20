969673dc50b90aae7de63fc947e35e9ecc00ef59
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-20 00:21:30 +0000

    added print_to_98() function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,27 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * print_to_98 - prints to 98
+ * @n: integer to start at
+ * Return: void
  */
 void print_to_98(int n)
 {
-	
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d,", n);
+			n--;
+		}
+	}
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d,", n);
+			n++;
+		}
+	}
+	printf("98\n");
 }
