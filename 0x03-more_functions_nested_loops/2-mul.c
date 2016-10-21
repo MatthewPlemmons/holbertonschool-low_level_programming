c8e8ddb2730d1a085a55ba89de04544746093311
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-21 01:39:06 +0000

    added program to multiply two integers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first factor.
+ * @b: second factor.
+ *
+ * Return: product of the two factors.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
