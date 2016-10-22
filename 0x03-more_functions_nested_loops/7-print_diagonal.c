87165aeaada41ccec8952e3d47a87e1837eb19e8
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-22 00:24:39 +0000

    added print_diagonal() program

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -10,11 +10,13 @@ void print_diagonal(int n)
 {
 	if (n > 0)
 	{
-		int i = n;
+		int i, j;
+
+		j = n;
 		while (n > 0)
 		{
-			i = (i - n) + 1;
-			while (i < n)
+			i = n;
+			while (i < j)
 			{
 				_putchar(' ');
 				i++;
