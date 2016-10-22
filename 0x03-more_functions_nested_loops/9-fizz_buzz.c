a1aeb3648d239f74394aca6aed1b2a80785234b5
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-22 04:24:15 +0000

    added fizz buzz function

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100, replacing multiples of 3 and 5 with Fizz and Buzz.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%i ", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
