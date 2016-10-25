551596f3dd3724ffe11119b560f4e361c919dabd
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-25 00:21:54 +0000

    added swap() function

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the value of two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *b;
+
+	*b = *a;
+	*a = c;
+}
