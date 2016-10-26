fbbf2b6c10c1b68215e833f47d46987d802dded5
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-26 22:26:56 +0000

    added puts2 function

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other char.
+ * @str: string input.
+ * Return: void
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
