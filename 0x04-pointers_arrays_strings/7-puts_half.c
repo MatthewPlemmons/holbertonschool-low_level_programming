d1833e788098abae057f6bfaf1cc2c7fb7bdc207
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-27 01:08:34 +0000

    edited puts_half() function

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,10 @@ void puts_half(char *str)
 	{
 		len++;
 	}
-	len = len / 2;
+	if (len % 2 == 0)
+		len = len / 2;
+	else
+		len = (len - 1) / 2;
 	while (str[len] != '\0')
 	{
 		_putchar(str[len]);
