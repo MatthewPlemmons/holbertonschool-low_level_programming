8c3bd807ed47c4ffc6073c67061a4e4ed6771c1c
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-27 00:09:53 +0000

    added print_array() function

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+
+/**
+ * print_array - prints n elements of an array of integers.
+ * @a: array input.
+ * @n: number of elements to print.
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%i", a[i]);
+		i++;
+		if (i < n)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
