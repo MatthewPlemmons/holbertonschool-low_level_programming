e8983994c247f11360d1f49d013d6753b2c1b38a
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-29 03:01:57 +0000

    added cap_string() function

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize all words of a string.
+ * @s: string input.
+ * Return: char pointer to string.
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+	char *c = s;
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!',
+			'?', '"', '(', ')', '{', '}'};
+
+	while (*s != '\0')
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{
+			i = 0;
+			for (i = 0; i < 13; i++)
+			{
+				if (*(s - 1) == sep[i])
+				{
+					*s = *s - ('a' - 'A');
+					break;
+				}
+			}
+		}
+		s++;
+	}
+	return (c);
+}
