a19065d4012464a61b9d73517616faf86b3ba4cd
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-29 05:21:55 +0000

    added _strncpy() function

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string.
+ * @dest: first input.
+ * @src: string to copy.
+ * @n: bytes of src to copy.
+ * Return: char pointer to dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *s = dest;
+	int i = 0;
+
+	while (*src != '\0' && i < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	while (i <= n)
+	{
+		*dest = '\0';
+		dest++;
+		i++;
+	}
+	return (s);
+}
