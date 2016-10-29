48a9bae80ca1d2e46a7454bf3b092b7d7fc74633
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-29 05:41:03 +0000

    edited _strncpy function

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -13,14 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 	char *s = dest;
 	int i = 0;
 
-	while (*src != '\0' && i < n)
+	while (*src != '\0' && i <= n)
 	{
 		*dest = *src;
 		dest++;
 		src++;
 		i++;
 	}
-	while (i < n)
+	while (i <= n)
 	{
 		dest++;
 		*dest = '\0';
