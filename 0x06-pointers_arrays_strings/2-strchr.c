17ce703f234be9f840105b0104b9c9334e12e18d
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-10-31 20:43:33 +0000

    added _strchr() function

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: string to search.
+ * @c: character to search for.
+ * Return: char pointer to first occurence of c.
+ * Or NULL if c is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (s);
+}
