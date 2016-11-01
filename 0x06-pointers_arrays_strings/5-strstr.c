b6ea89aece1572ef218bdc4b2aa88c5f94d31a79
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-01 04:00:49 +0000

    edited _strstr function

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,8 @@ char *_strstr(char *haystack, char *needle)
 	while (*haystack != '\0')
 	{
 		i = 0;
-		while (*(haystack + i) == *(needle + i))
+		while (*(haystack + i) == *(needle + i)
+		       && *(haystack + i) != '\0')
 		{
 			if (*(needle + i + 1) == '\0')
 			{
