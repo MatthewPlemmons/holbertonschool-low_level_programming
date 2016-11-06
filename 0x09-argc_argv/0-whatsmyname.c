1800a31e59a1b954c8a965e0338b9ecab65bed36
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-06 00:03:31 +0000

    added whatsmyname program

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * 
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+		return (0);
+	}
+	return (1);
+}
