60a27e30e238ad065a311f7892f1b53c99b668e5
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-08 07:06:18 +0000

    edited atoi function

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -16,17 +16,14 @@ int _atoi(char *s)
 	while (*(s + i))
 	{
 		if (*(s + i) == '-')
-		{
-			sign = -1;
-		}
+			sign *= -1;
+
 		if (*(s + i) >= '0' && *(s + i) <= '9')
 		{
 			num = num * 10 + (*(s + i) - '0');
+
 			if (*(s + i + 1) == ' ')
-			{
-				sign = 1;
 				break;
-			}
 		}
 		i++;
 	}
