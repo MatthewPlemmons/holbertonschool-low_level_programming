943c1cec4f5e8fc9443fa178ae94625cbe547566
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-08 06:48:14 +0000

    added atoi function

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * _atoi - converts string to integer.
+ * @s: string input.
+ * Return: int, converted string.
+ */
+
+int _atoi(char *s)
+{
+	int i, sign, num;
+
+	i = num = 0;
+	sign = 1;
+
+       	while (*(s + i))
+	{
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			num = num * 10 + (*(s + i) - '0');
+			if (*(s + i - 1) == '-')
+			{
+				sign = -1;
+			}
+			if (*(s + i + 1) == ' ')
+			{
+				sign = 1;
+				break;
+			}
+		}
+		i++;
+	}
+	num *= sign;
+	return (num);
+}
