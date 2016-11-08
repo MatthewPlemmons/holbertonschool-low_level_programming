c9c9e4c27da75b252cd735cd2ee68402dc51a1db
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-08 07:31:56 +0000

    added mul.c program

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - multiply two numbers together.
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int a;
+		int b;
+
+		a = _atoi(argv[1]);
+		b = _atoi(argv[2]);
+
+		printf("%d\n", a * b);
+		return (0);
+	}
+	else
+		printf("Error\n");
+		return (1);
+}
