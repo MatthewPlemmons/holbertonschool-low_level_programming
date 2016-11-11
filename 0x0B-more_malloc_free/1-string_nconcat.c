b8e3af2f1d59d41a8149360818dd825306267e4e
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-11 02:45:02 +0000

    edited string_nconcat function

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -29,9 +29,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *a;
 
 	i = 0;
-	len1 = _strlen(s1);
-	a = malloc((len1 + n) * sizeof(*a));
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = _strlen(s1);
+
+	a = malloc((len1 + n) * sizeof(*a));
 	if (!a)
 		return (NULL);
 
