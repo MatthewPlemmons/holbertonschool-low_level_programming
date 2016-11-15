083107a71f095db0865afb6acdb02c6f0f9c1a0e
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-15 22:39:02 +0000

    added print_dog fucntion

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name == NULL)
+			d->name = "(nil)";
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			d->owner = "(nil)";
+		printf("Owner: %s\n", d->owner);
+	}
+}
