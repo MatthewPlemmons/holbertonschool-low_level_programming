731ed54b7498cee36f4c84d3ace294511e569e1b
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-15 21:47:23 +0000

    added header file

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure for dog data.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ */
 struct dog
 {
 	char *name;
@@ -9,3 +15,4 @@ struct dog
 };
 
 #endif
+
