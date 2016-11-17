e22be5f5a9bb4027daac7a328dd45e8acc5d9cec
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-17 21:48:53 +0000

    added div by 0 error checking in main

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -19,8 +19,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+	if ((argv[2] == '/' || argv[2] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	op_func = (get_op_func(argv[2]));
 	if (op_func == NULL)
 	{
