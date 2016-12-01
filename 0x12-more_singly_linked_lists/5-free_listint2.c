467c21417be9714dad555783b13fb9952e4a6a48
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-01 00:44:55 +0000

    edited free_listint2 function

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -3,20 +3,19 @@
 
 /**
  * free_listint2 - frees a linked list.
- * @head:
- * Return:
+ * @head: start of list.
+ * Return: void
  */
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp0;
+	listint_t *temp;
 
-	temp0 = *head;
-	while (head)
+	while (*head)
 	{
-		*head = temp0;
-		temp0 = temp0->next;
-		*head = NULL;
-		free(*head);
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		temp = NULL;
 	}
 }
