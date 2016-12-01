5cd92f8bc529fce6ac59d4c486773efcff34c1a3
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-01 20:39:16 +0000

    added print_listint function

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -11,9 +11,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
-	for (i = 0; h; i++)
+	for (i = 0; h != NULL; i++)
 	{
-		printf("%i\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (i);
