ad2fcbb881a4ea59d239cdbe6e91b170eca09744
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-01 21:06:13 +0000

    edited free_listint2 function

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,17 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		temp = NULL;
+	}
 	while (*head)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
-	*head = NULL;
 }
