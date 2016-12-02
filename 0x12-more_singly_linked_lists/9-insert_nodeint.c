53049aa7d1c6807ecd8c2db261cce7010dc33491
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-02 00:33:16 +0000

    edited insert_nodeint

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 	{
 		if (temp->next == NULL)
 		{
-			new->next = NULL;
+			new->next = temp->next;
 			temp->next = new;
 			return (new);
 		}
