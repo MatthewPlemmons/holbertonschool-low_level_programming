7c5833eeb00ff23eda1d84ad702ef72560c506b9
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-06 07:43:46 +0000

    edited flip_bits function

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * flip_bits - return the number of bits different between n and m.
+ * @n: first unsigned integer input.
+ * @m: second unsigned integer input.
+ * Return: unsigned int, number of bits.
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
