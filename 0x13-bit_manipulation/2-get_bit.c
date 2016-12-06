4f2778f08e743976aff3b84cff6ce0642ea4bb0b
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-06 08:11:02 +0000

    edited get_bit

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -12,6 +12,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
+	if (index > sizeof(long) * 8)
+		return (-1);
 	i = (n & (1 << index) ? 1 : 0);
 	if (i <= 1)
 		return (i);
