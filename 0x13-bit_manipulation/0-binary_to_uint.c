4976811f3498c8ab25f08a9afadf9b96b441790b
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-06 01:17:05 +0000

    added binary_to_uint function

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts binary number to an unsigned int.
+ * @b: binary string.
+ * Return: unsigned int, converted number.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int total;
+
+	total = 0;
+	while (*b)
+	{
+		total <<= 1;
+		if (*b++ == '1')
+			total += 1;
+	}
+	return (total);
+
+}
