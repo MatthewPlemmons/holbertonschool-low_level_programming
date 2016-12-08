f4976b1b50c95cc4de85d523d21dc3d194e8654c
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-08 04:35:30 +0000

    added read_textfile function

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX standard output.
+ * @filename: file to be read.
+ * @letters: bytes to read and print.
+ * Return: ssize_t, bytes read.
+ *          0, if error.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n;
+	char buffer[1025];
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 00700);
+	if (fd == -1)
+		return (0);
+
+	n = read(fd, buffer, letters);
+	write(1, buffer, n);
+	close(fd);
+	return (n);
+}
