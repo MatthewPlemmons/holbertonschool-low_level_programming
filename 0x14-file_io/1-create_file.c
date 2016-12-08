e111b278ec6c8ef24b05e6556cd9034369eade2c
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-08 07:48:01 +0000

    more error checking for create_file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,7 +13,7 @@
 int create_file(const char *filename, char *test_content)
 {
 	size_t i;
-	int fd;
+	int fd, err_check;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,13 +21,19 @@ int create_file(const char *filename, char *test_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
+
+	if (test_content == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+
 	i = 0;
 	while (test_content[i])
 		i++;
-	if (test_content == NULL)
-		write(fd, NULL, 1);
-	else
-		write(fd, test_content, i);
+	err_check = write(fd, test_content, i);
 	close(fd);
+	if (err_check == -1)
+		return (-1);
 	return (1);
 }
