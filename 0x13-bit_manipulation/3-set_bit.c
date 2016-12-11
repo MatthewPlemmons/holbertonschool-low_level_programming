8bf6a11e36fdc9762cdbdc6aec94f79b676e71bc
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-12-11 18:35:39 +0000

    added error check to set_bit function

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -10,8 +10,8 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > sizeof(long) * 8)
+		return (-1);
 	*n |= (1 << index);
-	if (*n & (1 << index))
-		return (1);
-	return (-1);
+	return (1);
 }
