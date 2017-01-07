e715006d6be32113eb12a6ea0d386ce7596b485e
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-07 10:01:57 +0000

    function that prints list elements

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of dlistint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
