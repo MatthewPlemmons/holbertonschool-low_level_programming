8d10bb39de734ee87812b29709e044fb857a5bf4
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-07 14:49:27 +0000

    edited code

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -1,13 +1,10 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
  * add_dnodeint - adds new node to beginning of list
  * @head: double pointer to head of list
  * @n: data to store in node
- * Return: address of the new node
- *         or NULL, if failed
+ * Return: address of the new node, or NULL if failed.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
@@ -21,14 +18,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new->n = n;
 	new->prev = NULL;
-	if (!*head)
+	new->next = NULL;
+	if (*head)
 	{
-		new->next = NULL;
-		*head = new;
-		return (*head);
+		(*head)->prev = new;
+		new->next = *head;
 	}
-	(*head)->prev = new;
-	new->next = *head;
 	*head = new;
 	return (*head);
 }
