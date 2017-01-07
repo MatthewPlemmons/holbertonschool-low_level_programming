9b4dcc27184f405f626fd5658d7e1e610874265e
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-07 10:12:33 +0000

    prints number of nodes in list

---
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of nodes in a linked dlistint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+}
