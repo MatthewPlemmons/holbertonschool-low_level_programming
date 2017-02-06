839335fe5236bcdcb9be825ad4bd287adf805418
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-02-06 05:56:23 +0000

    add function to return index of a key

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * key_index - gets the index of a key.
+ * @key: input key.
+ * @size: size of the hash table.
+ * Return: index of key in the hash table.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{	
+	return (hash_djb2(key) % size);	
+}
