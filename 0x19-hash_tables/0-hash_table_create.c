ae2d0c0e4e788e66d15527cd4062d4aab7b77592
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-02-06 07:18:08 +0000

    change exit to return NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -13,11 +13,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	
 	h_table = malloc(sizeof(hash_table_t));
 	if (!h_table)
-		exit(EXIT_FAILURE);
+		return (NULL);
 	h_table->size = size;
 	h_table->array = malloc(sizeof(hash_node_t) * size);
 	if (!h_table->array)
-		exit(EXIT_FAILURE);
+		return (NULL);
 	for (i = 0; i < size; i++)
 		h_table->array[i] = 0;
 	return (h_table);
