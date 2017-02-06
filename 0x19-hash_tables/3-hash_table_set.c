ccc82701548ed7d4bd9e629a553d7df5f7ec6645
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-02-06 08:04:23 +0000

    change function call for hash

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -11,7 +11,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
 	unsigned long int hash_value;
-	const unsigned char *k;
 
 	if (!ht || !key || !value)
 		return (1);
@@ -19,11 +18,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 		return (1);
-	k = (const unsigned char *) strdup(key);
 	new->key = strdup(key);
 	new->value = strdup(value);
 
-	hash_value = hash_djb2(k) % ht->size;
+	hash_value = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[hash_value])
 		new->next = ht->array[hash_value];
