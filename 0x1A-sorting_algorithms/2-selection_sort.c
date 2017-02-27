beb88b17dd2ee76623c1a729e8955d04844f4d03
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-02-27 08:07:51 +0000

    files for task 2

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection sort - sort an array of ints in ascending order.
+ * @array: array of ints.
+ * @size: array size.
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k, tmp;
+	int **arr = &array;
+
+	i = j = k = tmp = 0;
+	while (i < size)
+	{
+		j = k = i;
+		while (j < size)
+		{
+			if (array[j] < array[k])
+				k = j;
+			j++;
+		}
+		if (i - k != 0)
+		{
+			tmp = (*arr)[i];
+			(*arr)[i] = (*arr)[k];
+			(*arr)[k] = tmp;
+			print_array(array, size);
+		}
+		i++;
+	}
+}
