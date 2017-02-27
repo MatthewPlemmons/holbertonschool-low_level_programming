faf72871d03ade640914cd67c9845658f28bb423
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-02-27 07:21:42 +0000

    edit bubble_sort

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -20,10 +20,8 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j - 1];
 				array[j - 1] = array[j];
 				array[j] = tmp;
+				print_array(array, size);
 			}
-			printf("%d, ", array[j - 1]);
-			if (j + 1 == size)
-				printf("%d\n", array[j]);
 		}
 	}
 }
