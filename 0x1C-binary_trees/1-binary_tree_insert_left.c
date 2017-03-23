ed2a4297dad292a13e1f53b877b31dc8287622f3
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-23 09:26:15 -0500

    add task 1 for inserting new node as left-child

---
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Insert node as the left-child of another node
+ * @parent: Parent of new node
+ * @value: Value for new node to hold
+ * Return: Pointer to new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new, *temp;
+
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
+	temp = parent->left;
+	parent->left = new;
+	new->left = temp;
+	if (temp)
+		temp->parent = new;
+	return (new);
+}
