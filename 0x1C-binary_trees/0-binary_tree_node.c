65c8911576e1cb1de4bceaa0812cfb8f6365bcfc
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-23 08:20:10 -0500

    add task 0 file for creating new node

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_node - Create binary tree node
+ * @parent: Parent of created node
+ * @value: Integer to store in node
+ * Return: New node, or NULL on failure.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+        new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	new->left = new->right = NULL;
+	return (new);
+}
