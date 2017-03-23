03187b87fccad629cbbf4d86628578f5296e2f30
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-23 10:52:22 -0500

    add task 5 files to check if node is root

---
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Check if node is root.
+ * @node: Node to check
+ * Return: int, 1 if node is root, 0 otherwise.
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node || node->parent)
+		return (0);
+	return (1);
+}
