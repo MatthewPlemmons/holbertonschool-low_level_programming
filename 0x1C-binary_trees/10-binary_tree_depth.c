796ef722c2818aae4944fe6c458a4011c314cd75
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-03-28 14:50:56 +0000

    add fucntion to bring depth of a given node in the tree

---
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Get depth of a node in binary tree.
+ * @tree: Node to measure
+ * Return: size_t depth of node, or 0 otherwise.
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	size_t depth;
+
+	if (!node)
+		return (0);
+	depth = 0;
+	if (node->parent)
+		depth = binary_tree_depth(node->parent) + 1;
+	return (depth);
+}
