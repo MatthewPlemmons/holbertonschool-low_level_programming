0e26d0ae58956e27a265b61b1481062e55cb383e
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-04-14 20:41:26 -0500

    update header file

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -9,6 +9,7 @@ int _linear_search(int *array, size_t size, size_t start, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+size_t calc_probe_position(int *array, size_t lo, size_t hi, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
