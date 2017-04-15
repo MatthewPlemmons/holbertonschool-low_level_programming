1998f19a710888a1f01900f3c28ef1c6c1ce6f81
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-04-14 22:38:46 -0500

    add check for 0 size

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -11,12 +11,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array)
+	if (!array || size == 0))
 		return (-1);
 	for (i = 0; i != size; i++)
 	{
 		printf("Value checked at array[%u] = [%d]\n",
-		       (unsigned int) i, (int) array[i]);
+		       (unsigned int) i, array[i]);
 		if (value == array[i])
 			return (i);
 	}
