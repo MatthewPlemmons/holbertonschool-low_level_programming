9bc8832e5ac7a805bcd2257be099b024bddcf043
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-05-12 08:52:12 -0500

    update header file

--- 0x1E-huffman_rb_trees/rb_trees.h
+++ 0x1E-huffman_rb_trees/rb_trees.h
@@ -1,14 +1,18 @@
+#ifndef _RB_TREES_H_
+#define _RB_TREES_H_
+
 /**
  * enum rb_color_e - Possible color of a Red-Black tree
  *
  * @RED: 0 -> Red node
  * @BLACK: 1 -> Black node
+ * @DOUBLE_BLACK: 2 -> Double-black node (used for deletion)
  */
 typedef enum rb_color_e
 {
 	RED = 0,
 	BLACK,
-    DOUBLE_BLACK
+	DOUBLE_BLACK
 } rb_color_t;
 
 /**
@@ -28,3 +32,5 @@ typedef struct rb_tree_s
 	struct rb_tree_s *left;
 	struct rb_tree_s *right;
 } rb_tree_t;
+
+#endif /* _RB_TREES_H_ */
