e37115f45620d730f6f27143638899a8fc3688a8
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-10-12 03:25:34 +0000

    replace while loop with for loop

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -6,17 +6,14 @@
  * @h: head of linked list.
  * Return: size_t, number of nodes in list.
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
-	i = 0;
-	while (h)
+	for (i = 0; h; ++i)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
 	}
 	return (i);
 
