32e584f803d307d27302b5cd48087fc87215a28c
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-10-28 22:23:33 +0000

    add function to print hash table

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -3,27 +3,24 @@
 /**
  * hash_table_print - prints a hash table.
  * @ht: hash table to print.
- * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
+	unsigned long int i, j;
 	hash_node_t *node;
-	
-	for (i = 0; i < ht->size; i++)
+
+	if (!ht)
+		return;
+	putchar('{');
+	for (i = j = 0; i < ht->size; ++i)
 	{
-		/* Should probably iterate over each element in the
-		   ht->array[], then check if each element is head of a linked
-		   list.  If so, follow node->next and print each
-		   node->value until you reach NULL end of list.
-		   Continue to next element of array.
-		*/
-		node = ht->array[i];
-		while (node->next)
-	        {
-			printf("%s\n", node->value);
-			node = node->next;
+		for (node = ht->array[i]; node; node = node->next)
+		{
+			if (j == 1)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			j = 1;
 		}
-		node = NULL;
 	}
+	printf("}\n");
 }
