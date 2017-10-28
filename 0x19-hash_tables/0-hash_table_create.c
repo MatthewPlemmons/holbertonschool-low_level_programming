26353123f4b74c200ef46e4a6ac41a70a9cfea76
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-10-28 23:29:40 +0000

    edit to malloc space for hash_node_t pointer

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!h_table)
 		return (NULL);
 	h_table->size = size;
-	h_table->array = malloc(sizeof(hash_node_t) * size);
+	h_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!h_table->array)
 		return (NULL);
 	for (i = 0; i < size; i++)
