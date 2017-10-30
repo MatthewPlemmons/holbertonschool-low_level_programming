56584b5d11f09c9404edb4d536f59cb5060b0457
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-10-30 03:37:24 +0000

    edit formatting and added null check

--- 0x12-more_singly_linked_lists/103-find_loop.c
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -7,23 +7,26 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-        listint_t *slow, *fast;
+	listint_t *slow, *fast;
 
-        slow = fast = head;
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                {
-                        slow = head;
-                        while (slow->next != fast->next)
-                        {
-                                slow = slow->next;
-                                fast = fast->next;
-                        }
-                        return (slow);
-                }
-        }
-        return (NULL);
+	if (!head)
+		return (NULL);
+
+	slow = fast = head;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow->next != fast->next)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
 }
