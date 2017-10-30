14e728dbccfe28b470ae4408958d9deb128824f1
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-10-30 03:32:23 +0000

    function to find a loop in a linked list

---
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - Finds the loop in a linked list.
+ * @head: head node of list
+ * Return: Address of node where list starts, otherwise NULL.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+        listint_t *slow, *fast;
+
+        slow = fast = head;
+        while (slow && fast && fast->next)
+        {
+                slow = slow->next;
+                fast = fast->next->next;
+                if (slow == fast)
+                {
+                        slow = head;
+                        while (slow->next != fast->next)
+                        {
+                                slow = slow->next;
+                                fast = fast->next;
+                        }
+                        return (slow);
+                }
+        }
+        return (NULL);
+}
